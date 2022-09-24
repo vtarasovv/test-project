0bc029ac939d83b0a99bbc551a7ca55a6b83cae3
Victor <vtarasoffff@gmail.com>
2022-09-24 15:47:37 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// let number = 5;
+// const leftBorderWidth = 1;
+
+// number = 10;
+// console.log(number);
+
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+// // console.log(obj.name);
+// console.log(obj ["name"]);
+
+// let arr = ['first', 'second', 'third', {}, []];
+// console.log(arr[1]);
+
+// alert ("hello");
+
+// const result = confirm("Are you here?");
+// console.log("result");
+
+// const answer = prompt("How are you?", "" );
+// console.log(typeof(answer));
+
+const answers = [];
+answers [0] = prompt("Как ваше имя?", "");
+answers [1] = prompt("Как ваша фамилия?", "");
+answers [2] = prompt("Cколько вам лет?", "");
+
+document.write (answers);
